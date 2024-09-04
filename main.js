7bf33a9e6df13c01ac4301946d0db2abb827de7d
MelchardSimeon <melchardsimeon71@gmail.com>
2024-09-04 09:15:59 +0800

    add game logic

---
+++ main.js
@@ -0,0 +1,19 @@
+
+
+let firstCard = 9;
+let secondCard = 12;
+let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
+
+if (sum < 21) {
+    console.log("Do you want to draw another card?")
+} else if (sum === 21) {
+    console.log("congrats, you won!")
+    hasBlackJack = true;
+} else {
+    console.log('byebye!')
+    isAlive = false;
+}
+
+console.log(isAlive)
