787e9a56459032223ae02c20fcbed14ddc25c8d4
MelchardSimeon <melchardsimeon71@gmail.com>
2024-09-05 00:43:52 +0800

    create new card function

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 let startButton = document.getElementById("startButton").addEventListener("click", startGame);
 let firstCard = 9;
 let secondCard = 12;
+
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true; 
@@ -13,6 +14,8 @@ let newCardButton = document.getElementById("new-card-button").addEventListener(
 
 
 function startGame() {
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+    sumEl.textContent = "Sum: " + sum;
         
     if (sum < 21) {
         message = "Do you want to draw another card?"
@@ -24,9 +27,13 @@ function startGame() {
         isAlive = false;
     }
         messageEl.textContent = message;
-        sumEl.textContent = "Sum: " + sum;
-        cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
     }
 
 
-    
+    function newCard() {
+        let card = 10;
+        sum += card;
+        startGame()
+    }
+
+    console.log(newCardButton)
