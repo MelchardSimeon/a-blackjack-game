9f8c81363a182dc5f5076bd139ceb7ecab6e084b
MelchardSimeon <melchardsimeon71@gmail.com>
2024-09-05 18:54:50 +0800

    add random function generate random card

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
 let startButton = document.getElementById("startButton").addEventListener("click", startGame);
-let firstCard = 9;
-let secondCard = 12;
+let firstCard = getRandomCard();
+let secondCard = getRandomCard();
 let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
@@ -12,17 +12,20 @@ let sumEl = document.getElementById("sum-el")
 let cardsEl = document.querySelector(".cards-el")
 let newCardButton = document.getElementById("new-card-button").addEventListener("click", newCard);
 
+function getRandomCard() {
+    randomNumber = Math.random() * 100
+    return Math.floor(randomNumber)
+}
+
 function startGame() {
     runGame();
 }
-
 
 function runGame() {
         cardsEl.textContent = "Cards: "
     for (let i = 0; i < cards.length; i++) {
         cardsEl.textContent += cards[i] + " ";
     }
-
     sumEl.textContent = "Sum: " + sum;
         
     if (sum < 21) {
@@ -37,9 +40,8 @@ function runGame() {
         messageEl.textContent = message;
     }
 
-
     function newCard() {
-        let card = 10;
+        let card = getRandomCard();
         sum += card;
         cards.push(card)
         console.log(cards)
